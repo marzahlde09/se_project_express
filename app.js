3b243588847244316b2c84c32fdacc19b7539d7d
Daniel Marzahl <marzahlde09@gmail.com>
2024-02-07 18:54:44 -0600

    add crash test code

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ const routes = require("./routes");
 
 app.use(requestLogger);
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 app.use(routes);
 
 app.use(errorLogger);
